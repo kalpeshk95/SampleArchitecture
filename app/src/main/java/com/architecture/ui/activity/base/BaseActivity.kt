f233703d495591b06776da8995aa9bc3361beb29
Kalpesh Kulye <kalpesh@agrahyah.com>
2021-02-24 17:37:02 +0530

    remove unused code

--- app/src/main/java/com/architecture/ui/activity/base/BaseActivity.kt
+++ app/src/main/java/com/architecture/ui/activity/base/BaseActivity.kt
@@ -1,21 +1,9 @@
 package com.architecture.ui.activity.base
 
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.provider.Settings
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.qifan.powerpermission.Permission
-import com.qifan.powerpermission.PowerPermission
-import com.qifan.powerpermission.data.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import timber.log.Timber
 import kotlin.coroutines.CoroutineContext
 
 abstract class BaseActivity : AppCompatActivity(), CoroutineScope {
